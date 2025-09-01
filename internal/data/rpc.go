4f9aa427c6e665fe6518a433454fec046b563171
Eone <2647926306@qq.com>
2025-09-01 19:09:06 +0800

    修改为服务发现

--- internal/data/rpc.go
+++ internal/data/rpc.go
@@ -3,6 +3,8 @@ package data
 import (
 	"context"
 	"demo/internal/conf"
+	"fmt"
+	registry "github.com/go-kratos/kratos/contrib/registry/nacos/v2"
 	"github.com/go-kratos/kratos/v2/log"
 	transgrpc "github.com/go-kratos/kratos/v2/transport/grpc"
 	"github.com/nacos-group/nacos-sdk-go/clients"
@@ -41,7 +43,7 @@ func NewGRPCClient(c *conf.Server, logger log.Logger) *GRPCClient {
 			ServerConfigs: sc,
 		},
 	)
-	conn, err := NewRpcConn(cli, "demoserveice.NewsService", "news")
+	conn, err := NewRpcConn(cli, "demoservice.grpc", "news")
 	if err != nil {
 		log.Error(err.Error())
 	}
@@ -51,14 +53,14 @@ func NewGRPCClient(c *conf.Server, logger log.Logger) *GRPCClient {
 
 	return d
 }
-func NewRpcConn(cli naming_client.INamingClient, servicename string, group string) (*grpc.ClientConn, error) {
-	// 创建 gRPC 连接
+func NewRpcConn(cli naming_client.INamingClient, serviceName string, group string) (*grpc.ClientConn, error) {
 	conn, err := transgrpc.DialInsecure(
 		context.Background(),
-		transgrpc.WithEndpoint("localhost:9001"), // 使用与 grpcurl 相同的地址
-		transgrpc.WithTimeout(10*time.Second),    // 增加超时时间
+		transgrpc.WithEndpoint(fmt.Sprintf("discovery:///%s", serviceName)),
+		//transgrpc.WithEndpoint("127.0.0.1:9001"),
+		transgrpc.WithDiscovery(registry.New(cli, registry.WithGroup(group))),
+		transgrpc.WithTimeout(20*time.Second),
 	)
-
 	if err != nil {
 		log.Errorf("failed to dial grpc server: %v", err)
 		return nil, err
